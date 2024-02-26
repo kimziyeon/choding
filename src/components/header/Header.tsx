eb567466085589c98bf0a03acc8443671a773925
dragon1431 <152239629+dragon1431@users.noreply.github.com>
2024-02-26 12:11:43 +0900

    헤더 퀴즈 link 연결

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import Logo from '@/essets/LOGO.svg';
 import Menu from '@/essets/menu.svg';
 import User from '@/essets/user.svg';
+import HeaderMenu from './components/HeaderMenu';
 
 import './header.scss';
 
 export default function Header() {
+    const [isOnHeaderMenu, setIsOnHeaderMenu] = useState(false);
+
+    const onClickMenu = () => {
+        setIsOnHeaderMenu(true);
+    }
+
     return (
         <header id="mainHeader">
+            <HeaderMenu />
             <div className='headerTOP'>
                 <div className='leftSide'>
-                    <Image src={Menu} alt='Logo' width={20} height={20}></Image>
+                    <Image src={Menu}
+                        alt='Logo'
+                        width={20}
+                        height={20}
+                        onClick={onClickMenu} />
                 </div>
                 <h1 className='titleLogo'>
                     <Link href='/' className='logo'>
@@ -20,16 +34,16 @@ export default function Header() {
                     </Link>
                 </h1>
                 <div className='rightSide'>
-                    <Link href='/signUp' className='signUp'>
+                    <Link href='/question' className='signUp'>
                         <Image src={User} alt='Logo' width={50} height={25}></Image>
                     </Link>
                 </div>
             </div>
-            <nav className='headerGNB'>
+            {/* <nav className='headerGNB'>
                 <Link href='/community'>커뮤니티 홈</Link>
                 <Link href='/community/QnA'>Q&A</Link>
                 <Link href='/community/myProject'>내 프로젝트</Link>
-            </nav>
+            </nav> */}
         </header>
     );
 }
