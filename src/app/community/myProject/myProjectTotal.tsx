38480f6f48270885133264f1cd53280f0e41b45a
SSkkky <worte5633@gmail.com>
2024-02-28 14:45:48 +0900

    route 업데이트

--- src/app/community/myProject/myProjectTotal.tsx
+++ src/app/community/myProject/myProjectTotal.tsx
@@ -3,47 +3,32 @@
 
 import axios, { AxiosResponse } from "axios";
 import { useState, useEffect } from "react";
+import serverStore from '@/lib/server/serverStore';
 import { myProjectPostType } from '@/types/datatype';
 import './myProjectTotal.scss';
 import FigureComponent from "./components/FigureComponent";
 import WriteMyProject from './components/WriteButton/WriteMyProject';
 
-export default function Home() {
+export default function MyProjectTotal() {
   const [result, setResult] = useState([]);
   const [postData, setPostData] = useState<myProjectPostType | null>(null);
   const [submitting, setSubmitting] = useState(false);
 
   async function dataCrl(type: string, idx?: number) {
-    let res: AxiosResponse | null = null;
-
-    switch (type) {
-      case 'all':
-        res = await axios.get('/api/mongodb');
-        break;
-      case 'insert':
-        res = await axios.post('/api/mongodb', postData);
-        break;
-      case 'delete':
-        res = await axios.delete(`/api/mongodb/${idx}`);
-        break;
-      case 'put':
-        res = await axios.put(`/api/mongodb/${idx}`, postData);
-        break;
-    }
+    const res = await serverStore(type, 'myProject');
 
     if (res !== null) {
-      console.log(res.data);
       setResult(res.data);
     }
   }
 
   useEffect(() => {
-    dataCrl('all')
+    dataCrl('get')
   }, [])
 
   useEffect(() => {
     if (submitting && postData !== null) {
-      dataCrl('insert');
+      dataCrl('post');
       setSubmitting(false);
     }
   }, [postData, submitting]);
@@ -53,15 +38,17 @@ export default function Home() {
     const formdata = new FormData(e.currentTarget);
     let writeData: any = Object.fromEntries(formdata.entries());
 
+    {/* 
     const newData: myProjectPostType = {
       postId: Date.now(),
       title: writeData.title,
       content: writeData.content,
       authorId: writeData.authorId,
       token: 'token',
-      date: new Date().getTime(),
+      date: '2024년 2월 28일',
       comments: []
     };
+    */}
 
     await setPostData(newData);
     setSubmitting(true);
