242580d89f6e39e161d7914fa685244212f8b3e3
KimJinWoo <krlawls1@gmail.com>
2024-02-28 17:51:22 +0900

    QnA 게시글작성페이지랑 메인페이지 하던중

--- src/app/community/QnA/page.tsx
+++ src/app/community/QnA/page.tsx
@@ -2,28 +2,19 @@
 
 import { useRef, useState } from 'react';
 import "./QnA.scss";
+import allPage from './allPage';
+import dynamic from 'next/dynamic';
+
+const AllPage = dynamic(() => import('./allPage'), { ssr: false });
 
 export default function QnA() {
     return (
     <section id="QnAMain" className="contPadding">
         <div className="QnAHeader">
-            
+            <button>전체 질문</button>
+            <button>내 질문</button>
         </div>
-        <div className='QnAContentContainer'>
-            <div className='QuestionBox'>
-                <div className='QuestionText'>
-                    <h2>[제목]농담농담[제목]</h2>
-                    <p>질문 내용질문 내용질문 내용질문 질문 내용질문 내용질문 내용질문 질문 내용질문 내용질문 내용질문 질문 내용질문 내용질문 내용질문 질문 내용질문 내용질문 내용질문 질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용질문 내용</p>
-                    <div className='QnAInfo'>
-                        <p>이름</p>
-                        <p>댓글 수 0</p>
-                    </div>
-                </div>
-                <div className='QuestionImg'>
-                    <img src="" alt="" />
-                </div>
-            </div>
-        </div>
+        <AllPage/>
     </section>
     );
 }
