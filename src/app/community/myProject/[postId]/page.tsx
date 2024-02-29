6ac6b17ff45a7ab51fb39db354cdf6a9c95602c1
SSkkky <worte5633@gmail.com>
2024-02-29 16:58:58 +0900

    detail 데이터 불러오기

--- src/app/community/myProject/[postId]/page.tsx
+++ src/app/community/myProject/[postId]/page.tsx
@@ -1,15 +1,35 @@
 "use client";
-import { useEffect } from 'react';
-import './MyProjectDetail.scss'
+import { useEffect, useState } from 'react';
 import { myProjectStore } from '@/app/community/myProject/context/myProject';
+import serverStore from '@/lib/server/serverStore';
+import './MyProjectDetail.scss'
 
 export default function MyProjectDetail({ params }: any) {
-    const { result } = myProjectStore();
-    console.log(params.postId)
+  const [result, setResult] = useState();
+  
+  async function dataCrl(type: string, postId: number) {
+        const res = await serverStore(type, 'myProject', null, postId);
+    
+        if (res !== null) {
+            console.log('--------res---------')
+            console.log(res.data)
+            // setResult(res.data);
+        }
+      }
+    
+    useEffect(() => {
+          console.log(params.postId)
+          dataCrl('detail', params.postId)
+      }, [params.postId])
+    
+      if (!Array.isArray(result)) {
+        return <div id="Loading">~ 로딩중입니다 ~</div>;
+      }
+      console.log(result)
 
     return (
         <section id="MyProjectDetail">
-
+            디테일 페이지
         </section>
     );
 }
