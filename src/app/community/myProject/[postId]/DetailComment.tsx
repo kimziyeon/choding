e332a8ab7f22e71949048005dcb19e6feafe166a
SSkkky <worte5633@gmail.com>
2024-03-01 17:59:14 +0900

    댓글 유저 프로필 사진

--- src/app/community/myProject/[postId]/DetailComment.tsx
+++ src/app/community/myProject/[postId]/DetailComment.tsx
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import Image from 'next/image';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import { myProjectPostType } from '@/types/datatype';
 import './DetailComment.scss'
@@ -27,14 +28,15 @@ export default function DetailComment({ result }: DetailCommentProps) {
                     const itemCreatedDate = dayjs(item.date, 'YYYY년 MM월 DD일');
                     if (!itemCreatedDate.isValid()) {
                         console.error(`Invalid date format for comment at index ${i}:`, item.date);
-                        return null;
+                        return '0';
                       }
                       const diffInDaysForItem = now.diff(itemCreatedDate, 'day');
                       
                     return <div className='mpdComment' key={i}>
-                      <div className='top'>
-                        <p className='mydUserId'>{item.userId}</p>
-                        <p className='mydCommentDate'>{diffInDaysForItem}일 전
+                        <div className='top'>
+                            <p className='mydUserPic'><Image src="https://blog.kakaocdn.net/dn/czXMO9/btrVoGszPDd/HXN5TVHM4d6AbXlQN0tdkK/img.jpg" width={36} height={36} alt="user profile"/></p>
+                            <p className='mydUserId'>{item.userId}</p>
+                            <p className='mydCommentDate'>{diffInDaysForItem}일 전
                         </p>
                       </div>
                       <div className='commentText'>{item.comment}</div>
