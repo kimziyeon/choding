e13029cde3e529ad4227537d0cd0d1a6cd4a16a0
SSkkky <worte5633@gmail.com>
2024-03-06 14:31:14 +0900

    회원가입 진행중

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,11 +1,22 @@
 "use client"
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { signUpType } from '@/types/user';
 import './signUp.scss';
 
 export default function SignUp() {
     const [isOnComplete, setComplete] = useState(false);
     const [email, setEmail] = useState('');
     const [domain, setDomain] = useState('google.com');
+
+    const { formState: { errors }, register, watch, setValue, handleSubmit: handleFormSubmit } = useForm<signUpType>({
+        defaultValues: {
+            id: '비회원',
+            password: '1234',
+            email: 'choding@choding.com',
+            imgSrc: 'https://firebasestorage.googleapis.com/v0/b/choding.appspot.com/o/writeImg%2FuserDefault.png?alt=media&token=21a6d29c-aa75-4f34-97a4-0935a44fbe2e'
+        },
+    });
 
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
@@ -15,10 +26,6 @@ export default function SignUp() {
         setDomain(e.target.value);
     };
 
-    const getFullEmail = () => {
-        return domain !== '직접입력' ? `${email}@${domain}` : email;
-    };
-
     return (
         <article id='signUp'>
             <header>
@@ -26,9 +33,9 @@ export default function SignUp() {
             </header>
             <form>
                 <div className='field'>
-                    <label htmlFor='userId'><b>아이디</b></label>
+                    <label htmlFor='userId'><b>닉네임</b></label>
                     <div className='input-group'>
-                        <input type="text" id="userId" placeholder='6~12자 이내' required />
+                        <input type="text" id="userId" placeholder='6자 이내' required {...register('id')} />
                         <button type='button'>중복확인</button>
                     </div>
                 </div>
@@ -41,13 +48,13 @@ export default function SignUp() {
                 <div className='field'>
                     <label htmlFor='userPasswordConfirm'><b>비밀번호 확인</b></label>
                     <div className='input-group'>
-                        <input type="password" id="userPasswordConfirm" placeholder='비밀번호를 다시 한 번 입력해주세요!' required />
+                        <input type="password" id="userPasswordConfirm" placeholder='비밀번호를 다시 한 번 입력해주세요!' required {...register('password')} />
                     </div>
                 </div>
                 <div className='field'>
                     <label htmlFor='userEmail'><b>이메일 주소</b></label>
                     <div className='input-group'>
-                        <input type="email" id="userEmail" placeholder='이메일을 입력해주세요' required />
+                        <input type="email" id="userEmail" placeholder='이메일을 입력해주세요' required {...register('email')} />
                         <button type='button'>중복확인</button>
                     </div>
                 </div>
