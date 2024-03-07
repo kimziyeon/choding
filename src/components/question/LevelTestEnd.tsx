8a672719bbca979ae63dc556a90f5eb83f2758f4
kimziyeon <aa40254037@gmail.com>
2024-03-07 14:46:10 +0900

    Doodoong

--- src/components/question/LevelTestEnd.tsx
+++ src/components/question/LevelTestEnd.tsx
@@ -7,12 +7,26 @@ import chocho from '@/essets/charactor/CHO.svg';
 import { useRouter } from 'next/navigation'
 import { useQuestion } from '@/context/questionStore';
 
-export default function LevelTestEnd() {
+export default function LevelTestEnd({ score }) {
     const router = useRouter();
     const { isOpenFunc } = useQuestion();
     function quizEnd() {
         isOpenFunc({ isOpen: false, isTest: false })
         router.push('/login');
+    }
+
+    const levelValue = () => {
+        if (score <= 2) {
+            return '초딩';
+        } else if (score <= 4) {
+            return '중딩';
+        } else if (score <= 6) {
+            return '고딩';
+        } else if (score <= 8) {
+            return '대딩';
+        } else if (score <= 10) {
+            return '직딩';
+        }
     }
 
 
@@ -26,9 +40,9 @@ export default function LevelTestEnd() {
                         </Image>
                     </div>
                     <div className='popUpSubTitle'>
-                        &nbsp;님의 총 점수는 &nbsp;점 입니다.
+                        &nbsp;님의 총 점수는 {score}점 입니다.
                         <div className='lvBox'>
-                            <span>Lv.&nbsp;</span>
+                            <span>Lv.{levelValue()}</span>
                         </div>
                         <div>
                             로그인을 통해 강의를 추천해드림
