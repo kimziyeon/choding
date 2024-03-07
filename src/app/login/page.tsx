4a9c87c39c3da94cd6bd3c1ec344c5e418306b12
SSkkky <worte5633@gmail.com>
2024-03-07 12:57:10 +0900

    로그인 수정중

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,42 +1,26 @@
 'use client';
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import serverStore from '@/lib/server/serverStore';
+import { useEffect, useState } from "react";
 import { signIn, useSession, signOut } from 'next-auth/react';
 import axios from "axios";
 import './login.scss'
 
 export default function Login() {
+    const [localUserInputEmail, setEmail] = useState('');
+    const [localUserInputPassword, setPassword] = useState('');
 
 
-    // useEffect(() => {
-    //     const code = location.search;
-    //     if (code) {
-    //         console.log('실행');
-    //         axios.post('/api', { param: code.substr(1) })
-    //             .then(res => {
-    //                 console.log(res.data);
-    //             });
-    //     }
-    // }, []);
-
-    // const goNaverLogin = () => {
-    //     window.location.href =
-    //         "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=0kFzpuisNF_m9ApdMXrl&redirect_uri=http://localhost:3000/login&state=asd2222222";
-    // };
-
-    // const handleGithubLogin = () => {
-    //     const router = useRouter();
-    //     router.push(
-    //         `${process.env.NEXT_PUBLIC_GITHUB_ORIGIN_URI}/login?client_id=${process.env.NEXT_PUBLIC_GITHUB_AUTHORIZE_CLIENT_ID}`
-    //     );
-    // };
-
+    // -------------------------------------------------- 로그인
+    // 소셜로그인 - 깃허브
     async function loginGit() {
         const result = await signIn("github", {
             redirect: true,
             callbackUrl: "/login",
         });
     }
+
+    // 소셜로그인 - 네이버
     async function loginNaver() {
         const result = await signIn("naver", {
             redirect: true,
@@ -45,38 +29,77 @@ export default function Login() {
         });
     }
 
-    async function aaaa() {
+    // 로컬 로그인 
+    async function loginLocal(bodyData) {
         const result = await signIn("credentials", {
             redirect: true,
+            body: JSON.stringify(bodyData),
             callbackUrl: "/login",
-            body: JSON.stringify({ id: 10000, name: '홍길동' })
         });
     }
-    //next auth
-    const { data: session, status } = useSession();
 
+
+
+    // -------------------------------------------------- 세션 저장 next auth
+    const { data: session, status } = useSession();
     // console.log(session);
 
 
+
+    // -------------------------------------------------- 로그아웃
     if (status === 'authenticated') {
         return <section>
             <p>Signed in as {session.user?.name}</p>
             <button onClick={() => { signOut() }}>logout</button>
         </section>
     }
+
+
+    // -------------------------------------------------- 로컬 로그인 검사
+    const inputEmailChange = (e) => { // input email 값 저장
+        setEmail(e.currentTarget.value);
+    }
+
+    const inputPasswordChange = (e) => { // input password 값 저장
+        setPassword(e.currentTarget.value);
+    }
+
+    const submitLogin = (e) => {
+        {/*
+            >> 해야할 일
+            db에서 LoginData를 get해와서 전체 데이터중에 localUserInputEmail이 있는지 체크한다.
+            아니면 ID나 PASSWORD가 일치하지 않습니다^^ 알림띄움
+            있으면 localUserInputPassword가 맞는지 체크한다.
+            맞으면 loginLocal(bodyData)에 값을 보냄
+            아니면 ID나 PASSWORD가 일치하지 않습니다^^ 알림띄움
+        */}
+
+        e.preventDefault();
+        console.log('submit 이메일은', localUserInputEmail);
+        console.log('submit 비밀번호는', localUserInputPassword);
+
+        const res = await serverStore('get', 'Login');
+        if (res !== null) {
+            setResult(res.data);
+        }
+        setOriginalData(result)
+
+    }
+
+
     return (
         <section>
             <div className="loginMain">
                 <h2>로그인</h2>
-                <button onClick={() => { aaaa() }}>aaaaa</button>
-                <form action="">
+
+                <form onSubmit={submitLogin}>
                     <div className="EmailContainer">
                         <label htmlFor="">이메일</label>
-                        <input type="text" id="userEmail" name="Email" />
+                        <input type="email" id="userEmail" name="Email" onChange={(e) => { inputEmailChange(e) }} />
                     </div>
                     <div className="passwordContainer">
                         <label htmlFor="">비밀번호</label>
-                        <input type="text" id="userPassaword" name="passaword" />
+                        <input type="password" id="userPassaword" name="passaword" onChange={(e) => { inputPasswordChange(e) }} />
                     </div>
                     <button type="submit">로그인</button>
                 </form>
