d1f996053dfa71aa17c2ac96731eef63bf5180cb
SSkkky <worte5633@gmail.com>
2024-03-12 11:40:15 +0900

    검색 기능 완료

--- src/app/community/myProject/myProjectFilter.tsx
+++ src/app/community/myProject/myProjectFilter.tsx
@@ -1,17 +1,20 @@
 //src/app/community/myProject/MyProjectFilter.tsx
 "use client";
+import Image from "next/image";
 import { useEffect, useRef, useState } from 'react';
-import SearchInputSub from "@/components/searchSub/SearchInputSub";
 import './myProjectFilter.scss'
 import FilterComponent from './components/FilterComponent';
 import { myProjectStore } from "./context/myProject";
 import { myProjectPostType } from '@/types/datatype'
+import SearchIconSVG from '@/essets/searchSub.svg'
+import SearchInputSub from '@/components/searchSub/SearchInputSub';
 
 export default function MyProjectFilter() {
   const { setResult, result, originalData, setOriginalData } = myProjectStore();
   const [filteredData, setFilteredData] = useState<myProjectPostType[]>([]);
   const [activeOptions, setActiveOptions] = useState<string[]>([]);
   const [isOnButtonActive, setisOnButtonActive] = useState(false);
+  const [keyword, setKeyword] = useState('');
   const filterRef = useRef<HTMLHeadingElement | null>(null);
 
 
@@ -32,7 +35,6 @@ export default function MyProjectFilter() {
       }
     });
   }
-
 
 
   // --------------- 필터 시작 ----------------
@@ -60,14 +62,44 @@ export default function MyProjectFilter() {
 
   // --------------- 필터 끝 ----------------
 
+
+  // ---------------- 검색 ------------------
+  const inputSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.currentTarget.value);
+  }
+
+  const handleEnterKey = (e) => {
+    if (e.key === 'Enter') {
+      const filterData = result.filter((item) => {
+        for (const value of Object.values(item)) {
+          if (typeof value === "string" && value.toLowerCase().includes(keyword.toLowerCase())) {
+            return true;
+          }
+        }
+        return false;
+      })
+      setOriginalData(filterData)
+    }
+  }
+
+
   return (
     <section id="myProjectFilter" className="communityContainer" ref={filterRef}>
       <div className="myProjectFilterHeader">
         <h3 className="filterTitle" onClick={filterToggleBtn}>
           {isOnButtonActive ? '필터 정렬 ∨' : '필터 정렬 ∧'}
         </h3>
-        <label htmlFor="myProjectSearch">dd?</label>
-        <input type="text" id="myProjectSearch" />
+        <input type="text"
+          className='inputSearch'
+          placeholder='2글자 이상 입력해주세용'
+          onChange={inputSearch}
+          onKeyDown={handleEnterKey} />
+        <Image
+          src={SearchIconSVG}
+          alt="검색 아이콘"
+          width={20}
+          height={20}
+        />
       </div>
       <div className="containerContents">
         <FilterComponent
