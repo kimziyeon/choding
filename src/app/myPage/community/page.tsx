020eab947823222c9e41c913016e462898dbb67d
kimziyeon <aa40254037@gmail.com>
2024-03-15 09:43:25 +0900

    315

--- src/app/myPage/community/page.tsx
+++ src/app/myPage/community/page.tsx
@@ -4,9 +4,7 @@
 import '../mypage.scss';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
-import Link from 'next/link';
 import arrowLeftGrayDark from '@/essets/arrowLeftGrayDark.svg';
-import MyCommunityEmpty from './commponents/Empty';
 import MyCommunityContents from './commponents/Contents';
 
 export default function MyCommunity() {
