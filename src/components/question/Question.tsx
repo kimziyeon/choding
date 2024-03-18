1626a46409385c994eb9831084635f1874ebe6ee
kimziyeon <aa40254037@gmail.com>
2024-03-18 23:33:41 +0900

    레벨테스트 팝업,마이페이지 css 수정

--- src/components/question/Question.tsx
+++ src/components/question/Question.tsx
@@ -14,40 +14,12 @@ import { useQuestion } from '@/context/questionStore';
 export default function UserQuestion() {
 
     const { quiz, isOpenFunc } = useQuestion();
-    const [isOpen, setIsOpen] = useState(true);
 
     const handleClose = () => {
         isOpenFunc({ isOpen: false, isTest: false });
     };
 
-    const oneDayHandleClose = () => {
-        isOpenFunc({ isOpen: false, isTest: false });
-        localStorage.setItem('popupClosedTime', String(new Date().getTime()));
-    }
-
-
-    useEffect(() => {
-        const lastClosedTime = localStorage.getItem('popupClosedTime');
-        if (lastClosedTime) {
-            const oneDay = 24 * 60 * 60 * 1000; // milliseconds
-            const currentTime = new Date().getTime();
-            const timeDifference = currentTime - Number(lastClosedTime);
-
-
-            if (timeDifference < oneDay) {
-                isOpenFunc({ isOpen: false, isTest: false });
-                console.log(isOpen)
-            } else {
-                isOpenFunc({ isOpen: true, isTest: false });
-            }
-        }
-    }, []);
-
-
-
     if (!quiz.isOpen) return null;
-
-
 
     return (
         <>
@@ -69,10 +41,6 @@ export default function UserQuestion() {
                     }
 
                 </div>
-
-                <label className='popUpCloseBtn' onClick={oneDayHandleClose}>
-                    하루동안 열지 않기
-                </label>
             </section >
         </>
 
