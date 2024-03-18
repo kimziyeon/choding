d3c3b66365b3619b6eba2869051b6c14d7d157b8
kimziyeon <aa40254037@gmail.com>
2024-03-18 18:22:32 +0900

    팝업수정중

--- src/components/question/LevelTestStart.tsx
+++ src/components/question/LevelTestStart.tsx
@@ -13,7 +13,6 @@ export default function LevelTestStart({ startTest }: any) {
     const { data: session, status } = useSession();
     const [mypageData, setMypageData] = useState<userPointType>();
     const { quiz, isOpenFunc } = useQuestion();
-    const [isOpen, setIsOpen] = useState(true);
 
 
 
@@ -23,11 +22,6 @@ export default function LevelTestStart({ startTest }: any) {
         router.push('/myPage');
     }
 
-
-    function handleClose() {
-        setIsOpen(false);
-        localStorage.setItem('popupClosedTime', String(new Date().getTime()));
-    }
 
 
     useEffect(() => {
@@ -45,60 +39,41 @@ export default function LevelTestStart({ startTest }: any) {
 
 
 
-    useEffect(() => {
-        const lastClosedTime = localStorage.getItem('popupClosedTime');
-        if (lastClosedTime) {
-            const oneDay = 24 * 60 * 60 * 1000; // milliseconds
-            const currentTime = new Date().getTime();
-            const timeDifference = currentTime - Number(lastClosedTime);
-            if (timeDifference < oneDay) {
-                setIsOpen(false);
-            } else {
-                setIsOpen(true);
-            }
-        } else {
-            setIsOpen(true);
-        }
-    }, []);
-
-
 
     return (
-        <>
-            <div>
-                <div className='popUp01'>
-                    <div className='popUpContents'>
-                        <h3>레벨 테스트</h3>
-                        <div className='popUpSubTitle'>
+        <div>
+            <div className='popUp01'>
+                <div className='popUpContents'>
+                    <h3>레벨 테스트</h3>
+                    <div className='popUpSubTitle'>
 
-                            {status === 'authenticated' ? (
-                                <>
-                                    {mypageData?.level ?
-                                        (<div className='nameTitle'><span>{session?.user?.name}</span>님은 레벨테스트가<br></br>완료 되었어요</div>)
-                                        :
-                                        (<div className='nameTitle'><span>{session?.user?.name}</span>님에게 더 잘 맞는<br></br>강의를 추천해 드릴께요</div>)
-                                    }
-                                </>
+                        {status === 'authenticated' ? (
+                            <>
+                                {mypageData?.level ?
+                                    (<div className='nameTitle'><span>{session?.user?.name}</span>님은 레벨테스트가<br></br>완료 되었어요</div>)
+                                    :
+                                    (<div className='nameTitle'><span>{session?.user?.name}</span>님에게 더 잘 맞는<br></br>강의를 추천해 드릴께요</div>)
+                                }
+                            </>
 
-                            ) : (
-                                <div className='noNameTitle'>회원가입하면<br></br>맞춤 강의를 추천해드려요!</div>
-                            )}
+                        ) : (
+                            <div className='noNameTitle'>회원가입하면<br></br>맞춤 강의를 추천해드려요!</div>
+                        )}
 
-                            <div className='tagBox'>
-                                <span>#10초</span>
-                                <span>#OX퀴즈</span>
-                            </div>
+                        <div className='tagBox'>
+                            <span>#10초</span>
+                            <span>#OX퀴즈</span>
                         </div>
-
-                        {mypageData?.level ?
-                            <button className='popUpBtn' onClick={mypageGoGo}>내 레벨 확인하기</button>
-                            :
-                            <button className='popUpBtn' onClick={() => { startTest(2) }}>테스트 시작하기</button>
-                        }
-
                     </div>
+
+                    {mypageData?.level ?
+                        <button className='popUpBtn' onClick={mypageGoGo}>내 레벨 확인하기</button>
+                        :
+                        <button className='popUpBtn' onClick={() => { startTest(2) }}>테스트 시작하기</button>
+                    }
+
                 </div>
             </div>
-        </>
+        </div>
     )
 }
--- src/components/question/Question.tsx
+++ src/components/question/Question.tsx
@@ -14,11 +14,35 @@ import { useQuestion } from '@/context/questionStore';
 export default function UserQuestion() {
 
     const { quiz, isOpenFunc } = useQuestion();
-
+    const [isOpen, setIsOpen] = useState(true);
 
     const handleClose = () => {
         isOpenFunc({ isOpen: false, isTest: false });
     };
+
+    const oneDayHandleClose = () => {
+        isOpenFunc({ isOpen: false, isTest: false });
+        localStorage.setItem('popupClosedTime', String(new Date().getTime()));
+    }
+
+
+    useEffect(() => {
+        const lastClosedTime = localStorage.getItem('popupClosedTime');
+        if (lastClosedTime) {
+            const oneDay = 24 * 60 * 60 * 1000; // milliseconds
+            const currentTime = new Date().getTime();
+            const timeDifference = currentTime - Number(lastClosedTime);
+
+
+            if (timeDifference < oneDay) {
+                isOpenFunc({ isOpen: false, isTest: false });
+                console.log(isOpen)
+            } else {
+                isOpenFunc({ isOpen: true, isTest: false });
+            }
+        }
+    }, []);
+
 
 
     if (!quiz.isOpen) return null;
@@ -26,26 +50,32 @@ export default function UserQuestion() {
 
 
     return (
-        <section className={`question ${quiz.isOpen ? 'active' : ''}`}>
-            <div className='popUpBg'>
-                <button className='popUpX'
-                    onClick={handleClose}>
-                    <Image
-                        className='close'
-                        src={Close}
-                        alt='close Image'
-                        width={25} height={25}
-                    ></Image>
-                </button>
+        <>
 
-                {
-                    quiz.isTest ? <LevelTest /> : <Dailyquiz />
-                }
+            <section className={`question ${quiz.isOpen ? 'active' : ''}`}>
+                <div className='popUpBg'>
+                    <button className='popUpX'
+                        onClick={handleClose}>
+                        <Image
+                            className='close'
+                            src={Close}
+                            alt='close Image'
+                            width={25} height={25}
+                        ></Image>
+                    </button>
 
-            </div>
+                    {
+                        quiz.isTest ? <LevelTest /> : <Dailyquiz />
+                    }
 
+                </div>
 
+                <label className='popUpCloseBtn' onClick={oneDayHandleClose}>
+                    하루동안 열지 않기
+                </label>
+            </section >
+        </>
 
-        </section >
     );
 }
+
