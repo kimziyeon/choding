ec9089566e6bf1a1ebde427e823d3e06bf0b8d03
SSkkky <worte5633@gmail.com>
2024-03-19 16:56:16 +0900

    add metatag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,42 @@ import { SessionProvider } from 'next-auth/react';
 import UserQuestion from "@/components/question/Question";
 import { Suspense } from 'react';
 
-// export const metadata: Metadata = {
-//   title: "초딩 - 초보들의 코딩공부",
-//   description: "팀곰문곰팀의 초딩 웹사이트입니다.",
-// };
+export const metadata: Metadata = {
+  manifest: "/manifest.json",
+  metadataBase: new URL('https://choding.vercel.app/'),
+  title: "초딩 - 초보들의 코딩공부",
+  description: "팀곰문곰팀의 초딩 웹사이트입니다.",
+  openGraph: {
+    title: '초딩 - 초보들의 코딩공부',
+    description: '팀곰문곰팀의 초딩 웹사이트입니다.',
+    images: '/choding.png'
+  },
+  twitter: {
+    title: '초딩 - 초보들의 코딩공부',
+    description: '팀곰문곰팀의 초딩 웹사이트입니다.',
+    images: '/choding.png'
+  },
+  icons: {
+    icon: '/favicon-16x16.png',
+    apple: '/favicon-16x16.png',
+    shortcut: '/choding.png',
+    other: {
+      rel: 'apple-touch-icon-precomposed',
+      url: '/favicon-32x32.png',
+    }
+  },
+  keywords: ['next', 'react', 'routing'],
+  authors: [{ name: '팀 곰문곰' }],
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+    }
+  }
+};
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
